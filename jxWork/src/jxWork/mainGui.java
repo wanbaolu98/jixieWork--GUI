30b926aa81b5f57a7e111585358b8fec96f0d9f0
MrYaoxiuwan <43696284+MrYaoxiuwan@users.noreply.github.com>
2019-03-03 16:08:58 +0800

    添加了新界面以及表格模型

--- jxWork/src/jxWork/mainGui.java
+++ jxWork/src/jxWork/mainGui.java
@@ -62,8 +62,8 @@ public class mainGui extends JFrame {
 		
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 322, 635);
-		contentPane = new JPanel();
+		setBounds(100, 100, 843, 635);
+		contentPane = new JPanel(null);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
