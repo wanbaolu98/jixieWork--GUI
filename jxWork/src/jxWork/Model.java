277f8699a23cda047109de28853143e5a6d80057
MrYaoxiuwan <43696284+MrYaoxiuwan@users.noreply.github.com>
2019-03-05 10:27:12 +0800

    完成了数据的读取与显示，还差数据的计算与保存。

--- jxWork/src/jxWork/Model.java
+++ jxWork/src/jxWork/Model.java
@@ -18,20 +18,18 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Model 
 {
-	private static double[][] x ;
-	private static double[][] y ;
-	private static double[][] z;
-	public static void main(String[] args)
-	{
-//			jisuan();
-		 read("123","C:\\Users\\yao\\Desktop\\123.xls",2);
-		 pritshuzu();
+	private  Object[][] data ;
+	public Object[][] getX() {
+		return data;
 	}
-	public static void read(String mulu,String name,int pand) 
+	public Model() 
+	{
+		data= new Object[7][15];
+	}
+	public void read(String mulu,String name) 
 	{
 		try {
-//			InputStream in = new FileInputStream(new File("C:\\Users\\yao\\Desktop\\123.xls"));
-			InputStream in = new FileInputStream(new File(name));
+			InputStream in = new FileInputStream(new File(mulu+name));
 			String pdwenjian = (String) name.subSequence(name.length()-4, name.length());
 			Workbook wb = null;
 			if(pdwenjian.equals(".xls"))
@@ -44,96 +42,82 @@ public class Model
 			{
 				System.out.println("对不起");
 			}
-//			InputStream in = new FileInputStream(new File(mulu+name));
-//			XSSFWorkbook wb = new XSSFWorkbook(in);
-//			Workbook wb = new HSSFWorkbook(in);
-			Sheet sheet = wb.getSheetAt(0);	
+			Sheet sheet = wb.getSheetAt(0);	//返回该文件的第0张Sheet页
 			
-			double [][] zhans = new double[sheet.getRow(1).getLastCellNum()-1][7];
-			System.out.println(sheet.getRow(1).getLastCellNum());
-			int one=0;
-			int two=0;
+//			double [][] zhans = new double[sheet.getRow(1).getLastCellNum()-1][7];
+			int hang=0;
+			int lie=1;
 			if(sheet==null)
 			{
 				return;
 			}
+			//getLastRowNum()是返回该页有多少行
 			for(int row=1;row<=sheet .getLastRowNum();row++)
 			{
+				//getRow（x）是返回当前页的x列----对象。
 				Row hssrow = sheet.getRow(row);
 				if(hssrow==null) 
 				{
 					continue;
 				}
+				//getLastCellNum()返回该行有多少列。
 				for(int cell=1;cell<=hssrow.getLastCellNum();cell++) 
 				{
+					//getCell（x）是返回当前行的第x列----对象。
 					Cell hsshell = hssrow.getCell(cell);
 					if(hsshell==null) 
 					{
 						continue;
 					}
+					//getNumericCellValue()将该数据转换为一个double类型的数据。
 					double xzi = hsshell.getNumericCellValue();
-					zhans[one][two] = xzi;
-					if(one==zhans.length-1) 
+					data[hang][lie] = xzi;
+					if(lie==hssrow.getLastCellNum()-1) 
 					{
-						one=0;
+						lie=1;
 					}else 
 					{
-						one++;
+						lie++;
 					}
 				}
-				two++;
-			}
-			if(pand==1) 
-			{
-				x = zhans;
-			}else if(pand==2) 
-			{
-				y = zhans;
-			}else 
-			{
-				z = zhans;
+				hang++;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+//	public void pritshuzu() 
+//	{
+//		for(int i = 0;i<data.length;i++) 
+//		{
+//			for(int j = 0;j<data[i].length;j++) 
+//			{
+//				System.out.print(data[i][j]+"  ");
+//			}
+//			System.out.println();
+//		}
+//	}
 	
-	public static void jisuan() 
-	{
-		double jg_x=0;
-		double  jg_y=0;
-		double jg_z=0;
-		for(int i = 0;i<x.length;i++) 
-		{
-			for(int j=0;j<y.length;j++) 
-			{
-				for(int k = 0;k<z.length;k++) 
-				{
-//					jg_x = -x[i][1]-y[j][1]-z[k][1]-(y[j][0]*(x[i][6]-Sxy))-(z[k][0]*(x[i][5]+y[j][5]+Sxz);
-					jg_x = -x[i][1]-y[j][1]-z[k][1]-(y[j][0]*(x[i][6]-0))-(z[k][0]*(x[i][5]+y[j][5]+0));
-//					jg_y = -x[i][2]-y[j][2]-z[k][2]+(z[k][0]*(x[i][4]+y[j][4]-Syz));
-					jg_y = -x[i][2]-y[j][2]-z[k][2]+(z[k][0]*(x[i][4]+y[j][4]-0));
-					jg_z = -x[i][3]-y[j][3]+z[k][3]+(y[j][0]*x[i][4]);
-					System.out.println(jg_x+"----"+jg_y+"----"+jg_z);
-				}
-			}
-		}
-	}
-	
-	
-	
-	public static void pritshuzu() 
-	{
-		for(int i = 0;i<y.length;i++) 
-		{
-			for(int j = 0;j<y[i].length;j++) 
-			{
-				System.out.print(y[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
-	
-	
+//	public static void jisuan() 
+//	{
+//		double jg_x=0;
+//		double  jg_y=0;
+//		double jg_z=0;
+//		for(int i = 0;i<x.length;i++) 
+//		{
+//			for(int j=0;j<y.length;j++) 
+//			{
+//				for(int k = 0;k<z.length;k++) 
+//				{
+////					jg_x = -x[i][1]-y[j][1]-z[k][1]-(y[j][0]*(x[i][6]-Sxy))-(z[k][0]*(x[i][5]+y[j][5]+Sxz);
+//					jg_x = -x[i][1]-y[j][1]-z[k][1]-(y[j][0]*(x[i][6]-0))-(z[k][0]*(x[i][5]+y[j][5]+0));
+////					jg_y = -x[i][2]-y[j][2]-z[k][2]+(z[k][0]*(x[i][4]+y[j][4]-Syz));
+//					jg_y = -x[i][2]-y[j][2]-z[k][2]+(z[k][0]*(x[i][4]+y[j][4]-0));
+//					jg_z = -x[i][3]-y[j][3]+z[k][3]+(y[j][0]*x[i][4]);
+//					System.out.println(jg_x+"----"+jg_y+"----"+jg_z);
+//				}
+//			}
+//		}
+//	}
 }
